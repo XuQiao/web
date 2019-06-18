2fafb607ed63396264cb84cbaa916ec7a539b3c1
XuQiao <XuQiao>
2019-06-18 21:05:05 +0000

    add webmin conf

--- mysite/home/models.py
+++ mysite/home/models.py
@@ -1,12 +1,17 @@
 from django.db import models
+from modelcluster.fields import ParentalKey
 
-from wagtail.core.models import Page
+from wagtail.core.models import Page, Orderable
 from wagtail.core.fields import RichTextField
-from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.admin.edit_handlers import FieldPanel, MultiFieldPanel, InlinePanel, TabbedInterface, ObjectList
+from wagtail.images.edit_handlers import ImageChooserPanel
+from wagtail.search import index
 
 class HomePage(Page):
-    body = RichTextField(blank=True)
 
+	# Database fields
+    body = RichTextField(blank=True)
+    
     content_panels = Page.content_panels + [
         FieldPanel('body', classname="full"),
     ]
